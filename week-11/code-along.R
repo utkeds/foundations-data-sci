74683c4970ab0d68071ab1dcd0ed3e2ae0487631
Isabella Velasquez <ivelasq@gmail.com>
2023-10-30 11:00:35 -0700

    Update Week 11

--- week-11/code-along.R
+++ week-11/code-along.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 library(maps)
 
+bear_dat <-
+    read_csv(here::here("week-11", "data", "bears.csv"))
+
 world_map <- map_data("world")
-
-bear_dat <- read_csv(here::here("week-11", "data", "bears.csv"))
-
-table(bear_dat_grouped$place_country_name)
 
 bear_dat_grouped <-
     bear_dat %>% 
@@ -18,42 +17,24 @@ bear_dat_grouped <-
 bear_map <-
     left_join(world_map, bear_dat_grouped, join_by(region == place_country_name))
 
-ggplot(data = bear_map) +
-    geom_map(map = bear_map,
-             aes(
-                 x = long,
-                 y = lat,
-                 fill = n_bears,
-                 map_id = region
-             )) +
-    coord_fixed(1.3)
-
-# things to add
-## title
-## border
-## points from bear_dat
-## bear common_name as multicolored points
-## remove the legend with theme(legend.position = "none")
-## viridis scale_fill_viridis(option="magma")
+library(viridis)
 
 ggplot(data = bear_map) +
     geom_map(map = bear_map,
-             aes(
-                 x = long,
+             aes(x = long,
                  y = lat,
                  fill = n_bears,
-                 map_id = region
-             ),
+                 map_id = region),
              linewidth = 0.1,
              color = "white") +
-    coord_fixed(1.3) +
     geom_point(data = bear_dat,
                aes(x = longitude,
                    y = latitude,
                    color = common_name),
                alpha = 0.3,
                size = 0.1) +
-    theme(legend.position = "none") +
+    coord_fixed(1.3) +
     labs(title = "Bears around the world") +
-    scale_fill_viridis(option="magma") +
-    scale_color_viridis_d(option="magma")
+    theme_minimal() +
+    scale_fill_viridis(option = "magma") +
+    theme(legend.position = "none")
